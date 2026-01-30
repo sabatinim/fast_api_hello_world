0f35c48d1fa9709818d5ae748ce00f568434ccfc
Maverick <sabatini.m@gmail.com>
2026-01-31 00:22:18 +0100

    add dev container

--- app/main.py
+++ app/main.py
@@ -5,11 +5,11 @@ from app.infrastructure.app_controller import AppController
 from app.usecase.app_usecase import UseCase, ProductionUseCase
 
 
-def startup(use_case: UseCase = ProductionUseCase()):
+def startup(use_case: UseCase):
     app = FastAPI()
     AppController(app, use_case)
     return app
 
 
 if __name__ == "__main__":
-    uvicorn.run(startup(), host="0.0.0.0", port=8080)
+    uvicorn.run(startup(use_case=ProductionUseCase()), host="0.0.0.0", port=8080)
